cb92407cfc2011c901b8bdf0c121163f7dfd079f
flabbet <flubbet@gmail.com>
2021-05-30 10:30:42 +0200

    download wip

--- website/src/pages/download.tsx
+++ website/src/pages/download.tsx
@@ -4,7 +4,7 @@ import Layout from '@theme/Layout';
 function download(props){
     return (
     <Layout title="Download">
-        
+        <a href='//www.microsoft.com/store/apps/9NDDRHS8PBRN?cid=storebadge&ocid=badge'><img src='https://developer.microsoft.com/store/badges/images/English_get-it-from-MS.png' alt='English badge' style={{width: "284px", height: "104px"}}/></a>
     </Layout>);
 }
 
