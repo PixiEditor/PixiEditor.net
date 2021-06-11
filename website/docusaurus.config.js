f0d0d41145e1f21f45bb4da5455a727d28bb5c29
flabbet <flubbet@gmail.com>
2021-06-11 20:18:13 +0200

    Added google adsense

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -38,6 +38,11 @@ module.exports =
   baseUrl: '/', // Base URL for your project */
   projectName: "pixieditor.github.io",
   organizationName: 'PixiEditor',
+  scripts: [{
+    src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js",
+    async: true,
+    ["data-ad-client"]: "ca-pub-8372287848779618"
+  }],
   presets: [
     [
       '@docusaurus/preset-classic',
