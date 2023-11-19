20d1429fadbeb0dc413766a7b79f28a88d2c61fa
Krzysztof Krysiński <flubbet@gmail.com>
2023-11-19 21:06:11 +0100

    Added video

--- website/src/pages/colorpicker.tsx
+++ website/src/pages/colorpicker.tsx
@@ -15,7 +15,7 @@ function colorpicker(props) {
                     <h2 className="animate__animated animate__fadeInDown animate__delay-1s">
                         Likely the most customizable and advanced color picker for WPF and AvaloniaUI.
                     </h2>
-                    <img src="https://camo.githubusercontent.com/c94144c7c20d0c460f48073cd379b4eea47ea32a31fbc15ea89c4ed4ea65dcec/68747470733a2f2f692e696d6775722e636f6d2f3479734e3446652e706e67" alt="screenshot" className="animate__animated animate__fadeInDown highlighted-img" />
+                    <video muted autoPlay className="animate__animated animate__fadeInDown highlighted-img" src="/videos/colorpicker.mp4"/>
                 </div>
                 <div className="usage">
                     <h3>Installation</h3>
