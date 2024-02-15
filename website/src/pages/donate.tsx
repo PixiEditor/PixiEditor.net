7852d9c8802c8b539e719de781bc6cb05cf76e6f
Krzysztof Krysiński <flubbet@gmail.com>
2024-02-15 15:18:14 +0100

    Added supporter pack

--- website/src/pages/donate.tsx
+++ website/src/pages/donate.tsx
@@ -32,27 +32,41 @@ publishable-key="pk_test_51OjM1gHZ5wTFDpNNEJ7ilrSGIxVA8AHGD8EnFrynU86AmRrntFsmnt
 >
 </stripe-buy-button>`}}></div>
       </div>) : (
-      <div
-        className="one-time-donate-container"
+      <div className="one-time-donate-container">
+        <div
         dangerouslySetInnerHTML={{
           __html: `<stripe-buy-button
           buy-button-id="buy_btn_1OjMymHZ5wTFDpNNTBlRwfv3"
           publishable-key="pk_live_51OjM1gHZ5wTFDpNNLAmmwJ4nTbsKL2PWgx85DKCY1zamApjynsVCvOtw1ga7qoGCjKoztXrkEmyJh47TRMe1a7E7009AD3hvhw"
           >
-          </stripe-buy-button>`}}/>)
-          }
+          </stripe-buy-button>`}}/>
+          <h3>Or</h3>
+          <div>
+            <iframe src="https://store.steampowered.com/widget/2435860/" width="646" height="190"></iframe>
+          </div>
+      </div>
+          
+          )}
     </div>
   );
 };
 
 class Donate extends React.Component {
+
   componentDidMount(): void {
+
+    if (document.getElementById("stripe-js") || document.getElementById("stripe-js-2")) {
+      return;
+    }
+
     const script = document.createElement("script");
+    script.id = "stripe-js";
     script.src = "https://js.stripe.com/v3/pricing-table.js";
     script.async = true;
     document.body.appendChild(script);
 
     const script2 = document.createElement("script");
+    script2.id = "stripe-js-2";
     script2.src = "https://js.stripe.com/v3/buy-button.js";
     script2.async = true;
     document.body.appendChild(script2);
