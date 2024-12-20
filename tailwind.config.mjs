adbf8beca3e59445122e4e528aa929ce8f86f849
CPKreuz <cpkreu@gmail.com>
2024-12-20 20:28:49 +0100

    Adjusted spaing on index features

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,7 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	safelist: [
 		{
-			pattern: /(decoration|text)-pc-.*/
+			pattern: /(bg|text)-pc-.*/
 		}
 	],
 	theme: {
