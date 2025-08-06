9da8e4f8d98bf714a225a9148ebcc62eb01b0ab4
CPK <cpkreu@gmail.com>
2025-08-06 17:58:37 +0000

    Added cover images to rss feed

--- src/pages/blog.xml.ts
+++ src/pages/blog.xml.ts
@@ -2,6 +2,7 @@ import rss, { type RSSOptions } from '@astrojs/rss';
 import type { APIRoute } from 'astro';
 import { getCollection } from 'astro:content';
 import { getBlogPath } from "@content/contentHelper";
+import { getImage } from 'astro:assets';
 
 export const GET: APIRoute = async (context) => {
     const blog = await getCollection('blog');
@@ -12,16 +13,26 @@ export const GET: APIRoute = async (context) => {
         description: 'The blog for the universal open source 2D editor PixiEditor',
         site: new URL("/", context.site),
         xmlns: {
-            "atom": "http://www.w3.org/2005/Atom"
+            "atom": "http://www.w3.org/2005/Atom",
+            "media": "http://search.yahoo.com/mrss/" 
         },
-        items: blog.map((post) => {
+        items: await Promise.all(blog.map(async (post) => {
+            const image = post.data.cover ? await getImage({ src: post.data.cover!, format: "png" }) : undefined;
+            const imageData = image ? `<media:content 
+                    url="${new URL(image.src, context.site)}"
+                    medium="image"
+                    type="image/${image.options.format}"
+                    width="${image.options.width}"
+                    height="${image.options.height}" />` : undefined;
+
             return {
                 title: post.data.title,
                 pubDate: post.data.date,
                 description: post.data.description,
-                link: getBlogPath(post)
+                link: getBlogPath(post),
+                customData: imageData
             }
-        }),
+        })),
         customData: `<language>en-us</language>\n<atom:link href="${selfUrl}" rel="self" type="application/rss+xml" />`,
     };
 
