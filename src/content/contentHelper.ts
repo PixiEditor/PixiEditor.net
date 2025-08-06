09637a4c789f574dbee201a0a37f4db600bcb28f
CPK <cpkreu@gmail.com>
2025-08-06 16:44:12 +0000

    added getAuthors helper in contentHelper.ts

--- src/content/contentHelper.ts
+++ src/content/contentHelper.ts
@@ -1,9 +1,19 @@
-﻿import type { CollectionEntry } from "astro:content";
+﻿import { getCollection, type CollectionEntry } from "astro:content";
+
+const authors = await getCollection('authors');
 
 export function getBlogPath(post: CollectionEntry<'blog'>): string {
     const date = post.data.date;
 
     return `/blog/${date.getFullYear()}/${String(date.getMonth() + 1).padStart(2, '0')}/${String(date.getDate()).padStart(2, '0')}/${post.id}`;
+}
+
+export function getAuthors(post: CollectionEntry<'blog'>): CollectionEntry<'authors'>[] | undefined {
+    const data = post.data
+        .authors?.map(postAuthor => authors
+            .find(author => author.id == postAuthor.id));
+
+    return data?.filter(x => x).map(x => x!);
 }
 
 export function getDocsPath(doc: CollectionEntry<'docs'>) {
